4a274e8cf23b85430249cf9d0da1b6cdb279436a
Admiralhunter <hunterpalcich18@gmail.com>
2019-01-06 02:42:56 -0500

    Update Breast Cancer.py

--- Breast Cancer.py
+++ Breast Cancer.py
@@ -8,6 +8,7 @@ from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import RandomizedSearchCV
+from sklearn.preprocessing import StandardScaler
 
 
 plt.interactive(False)
@@ -20,7 +21,16 @@ data = pd.read_csv("D:\\breast-cancer-wisconsin-data\data.csv")
 
 
 data['diagnosis']=data['diagnosis'].map({'M':1,'B':0})
+labels = data['diagnosis']
+data.drop(['diagnosis'], axis=1, inplace=True)
 
+
+#Standardize the data
+scaler = StandardScaler()
+scaler.fit(data)
+scaler.transform(data)
+
+data['diagnosis'] = labels
 
 #Get's a 2D plot of all the correlations in the data
 corr = data.corr()
@@ -46,8 +56,10 @@ test_y = test_X.diagnosis   #output value of test dat
 train_X.drop(['diagnosis'], axis=1, inplace=True)
 test_X.drop(['diagnosis'], axis=1, inplace=True)
 
+
+
 #Define the model that we want to use
-clf = svm.SVC(verbose=True, tol=1e-5, gamma='scale')
+clf = svm.SVC(verbose=False, tol=1e-5, gamma='scale')
 
 # specify hyperparameters to cycle through and find the best one
 param_dist = {"C": np.linspace(0.01, 20, 50)}
@@ -55,7 +67,7 @@ param_dist = {"C": np.linspace(0.01, 20, 50)}
 # run randomized search
 n_iter_search = 20
 random_search = RandomizedSearchCV(clf, param_distributions=param_dist,
-                                   n_iter=n_iter_search, cv=5, verbose= 1, iid= True)
+                                   n_iter=n_iter_search, cv=5, verbose= 0, iid= True)
 
 #this applies the model with the best C value
 random_search.fit(train_X, train_y)
